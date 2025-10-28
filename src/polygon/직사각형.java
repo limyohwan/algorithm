2b994350d4b618b65264786b3dcb63bd74a36b0e
Yohwan Lim <dyghks7102@naver.com>
2025-10-28 20:12:50 +0900

    기하 1: 직사각형과 삼각형 1, 2, 3 문제

---
+++ src/polygon/직사각형.java
@@ -0,0 +1,14 @@
+package polygon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 직사각형 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        System.out.println(N * M);
+    }
+}
---
+++ src/polygon/직사각형에서탈출.java
@@ -0,0 +1,21 @@
+package polygon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 직사각형에서탈출 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int min = Math.min(Math.min(Math.min(x, y), w-x), h-y);
+
+        System.out.println(min);
+    }
+}
