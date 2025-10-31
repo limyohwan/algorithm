5bf8dab06319a9ec1f08dd9f5456835905c6dcb4
Yohwan Lim <dyghks7102@naver.com>
2025-10-31 16:05:05 +0900

    시간 복잡도 1, 2 문제

---
+++ src/complexity/알고리즘수행시간1.java
@@ -0,0 +1,10 @@
+package complexity;
+
+import java.io.IOException;
+
+public class 알고리즘수행시간1 {
+    public static void main(String[] args) throws IOException {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
