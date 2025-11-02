258b460f2d5a232b07fae8aa78b16c9124af2c93
Yohwan Lim <dyghks7102@naver.com>
2025-11-02 15:36:38 +0900

    시간 복잡도 4 문제 - 필요없는 부분 제거

--- src/complexity/알고리즘수행시간4.java
+++ src/complexity/알고리즘수행시간4.java
@@ -8,12 +8,7 @@ public class 알고리즘수행시간4 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long n = Long.parseLong(br.readLine());
-        if (n == 1) {
-            System.out.println(0);
-        } else {
-            System.out.println(n * (n -1) / 2);
-        }
-        
+        System.out.println(n * (n -1) / 2);
         System.out.println(2);
     }
 }
